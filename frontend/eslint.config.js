b3ed658d9a1eed56c1405b184550f3d8483ec6f6
Mikail P. <dremast1337@gmail.com>
2026-08-04 14:50:26 +0300

    rm old

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,11 +2,13 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,7 +18,6 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -24,4 +25,4 @@ export default [
       ],
     },
   },
-];
+);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import { Header } from './components/Header';
-import { CartDrawer } from './components/CartDrawer';
 import { HomePage } from './pages/HomePage';
 
 export const App: React.FC = () => {
   return (
     <Router>
       <div className="app-container">
-        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
-        <CartDrawer />
       </div>
     </Router>
   );
